a78e438a6acd243112f480d399d13ce62b54155c
Viole403 <m.maulana.fb26@gmail.com>
2025-12-19 21:41:26 +0700

    feat(treesitter): add file size check for parser disabling
    - Implement a function to disable highlighting for large files.
    - Set a maximum file size limit of 100 KB for parser availability checks.
    - Enhance performance by preventing unnecessary parsing on large files.

--- config/nvim/lua/plugins/treesitter.lua
+++ config/nvim/lua/plugins/treesitter.lua
@@ -15,9 +15,18 @@ return {
       },
       sync_install = false,
       auto_install = true,
+      ignore_install = {}, -- List of parsers to ignore installing
       highlight = {
         enable = true,
         additional_vim_regex_highlighting = false,
+        disable = function(lang, buf)
+          -- Disable for large files or if parser not available
+          local max_filesize = 100 * 1024 -- 100 KB
+          local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+          if ok and stats and stats.size > max_filesize then
+            return true
+          end
+        end,
       },
     }
   end
