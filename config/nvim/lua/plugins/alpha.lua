0ef4232d6dd4a9d042b7f51e404ea8ac50ad9c63
Viole403 <m.maulana.fb26@gmail.com>
2025-12-19 20:38:27 +0700

    refactor(nvim): update alpha dashboard button labels
    - Rename 'Settings' to 'Configuration'
    - Rename 'Recent Files' to 'Recent files'
    - Remove extra space in 'New File' command

--- config/nvim/lua/plugins/alpha.lua
+++ config/nvim/lua/plugins/alpha.lua
@@ -6,11 +6,11 @@ return {
     dashboard.section.header.val = { "Neovim" }
     dashboard.section.buttons.val = {
       dashboard.button("f", "Find File", ":Telescope find_files<CR>"),
-      dashboard.button("n", "New File", ":ene <BAR> startinsert <CR>"),
+      dashboard.button("n", "New File", ":ene <BAR> startinsert<CR>"),
       dashboard.button("p", "Projects", ":Telescope projects<CR>"),
-      dashboard.button("r", "Recent Files", ":Telescope oldfiles<CR>"),
+      dashboard.button("r", "Recent files", ":Telescope oldfiles<CR>"),
       dashboard.button("t", "Find Text", ":Telescope live_grep<CR>"),
-      dashboard.button("s", "Settings", ":e $MYVIMRC<CR>"),
+      dashboard.button("c", "Configuration", ":e $MYVIMRC<CR>"),
       dashboard.button("q", "Quit", ":qa<CR>"),
     }
     require("alpha").setup(dashboard.config)
