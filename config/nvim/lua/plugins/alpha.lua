80d9de58ced864c118812901231cb115df0860c5
Viole403 <m.maulana.fb26@gmail.com>
2025-12-19 15:18:29 +0700

    feat(alpha): add additional buttons to dashboard
    - Added buttons for "New File", "Recent Files", "Find Text", "Projects", and "Settings" to enhance user navigation.
    - Improved accessibility to frequently used commands directly from the dashboard.

--- config/nvim/lua/plugins/alpha.lua
+++ config/nvim/lua/plugins/alpha.lua
@@ -6,6 +6,11 @@ return {
     dashboard.section.header.val = { "Neovim" }
     dashboard.section.buttons.val = {
       dashboard.button("f", "Find File", ":Telescope find_files<CR>"),
+      dashboard.button("n", "New File", ":ene <BAR> startinsert <CR>"),
+      dashboard.button("r", "Recent Files", ":Telescope oldfiles<CR>"),
+      dashboard.button("t", "Find Text", ":Telescope live_grep<CR>"),
+      dashboard.button("p", "Projects", ":Telescope projects<CR>"),
+      dashboard.button("s", "Settings", ":e $MYVIMRC<CR>"),
       dashboard.button("q", "Quit", ":qa<CR>"),
     }
     require("alpha").setup(dashboard.config)
