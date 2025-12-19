cea48c8f909ac4e72fadfb2fb31d213bc28a1ca5
Viole403 <m.maulana.fb26@gmail.com>
2025-12-19 20:38:39 +0700

    refactor(nvim): improve dadbod lazy loading
    - Set vim-dadbod-completion to lazy load by filetype
    - Load only for sql, mysql, plsql files

--- config/nvim/lua/plugins/dadbod.lua
+++ config/nvim/lua/plugins/dadbod.lua
@@ -7,7 +7,7 @@ return {
     "kristijanhusak/vim-dadbod-ui",
     dependencies = {
       "tpope/vim-dadbod",
-      "kristijanhusak/vim-dadbod-completion",
+      { "kristijanhusak/vim-dadbod-completion", ft = { "sql", "mysql", "plsql" }, lazy = true },
     },
     cmd = {
       "DBUI",
