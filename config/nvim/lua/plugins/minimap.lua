d25ee83d78dc1d0f468bf45af948c40d7e70171e
Viole403 <m.maulana.fb26@gmail.com>
2025-12-19 22:05:44 +0700

    refactor(minimap): update initialization method for configuration
    - Changed the configuration method from `config` to `init` for the minimap plugin.
    - Updated the way minimap settings are applied using `vim.cmd` for better compatibility.

--- config/nvim/lua/plugins/minimap.lua
+++ config/nvim/lua/plugins/minimap.lua
@@ -4,12 +4,12 @@ return {
   build = "cargo install --locked code-minimap",
   cmd = { "Minimap", "MinimapClose", "MinimapToggle", "MinimapRefresh", "MinimapUpdateHighlight" },
   lazy = false,
-  config = function()
-    vim.g.minimap_width = 10
-    vim.g.minimap_auto_start = 1
-    vim.g.minimap_auto_start_win_enter = 1
-    vim.g.minimap_highlight_search = 1
-    vim.g.minimap_git_colors = 1
+  init = function()
+    vim.cmd("let g:minimap_width = 10")
+    vim.cmd("let g:minimap_auto_start = 1")
+    vim.cmd("let g:minimap_auto_start_win_enter = 1")
+    vim.cmd("let g:minimap_highlight_search = 1")
+    vim.cmd("let g:minimap_git_colors = 1")
     vim.g.minimap_block_filetypes = { "NvimTree", "TelescopePrompt", "alpha", "dashboard" }
   end,
 }
