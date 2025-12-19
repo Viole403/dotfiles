eb800719315f6d3fa233f268967ca798b16ac34f
Viole403 <m.maulana.fb26@gmail.com>
2025-12-19 13:49:17 +0700

    feat(options): conditionally set clipboard configuration
    - Wrap clipboard configuration in a version check for Neovim 0.10 or higher.
    - Ensure compatibility with older versions by preventing clipboard settings from being applied if the version requirement is not met.

--- config/nvim/lua/core/options.lua
+++ config/nvim/lua/core/options.lua
@@ -1,15 +1,18 @@
 -- core/options.lua
-vim.g.clipboard = {
-  name = "osc52",
-  copy = {
-    ["+"] = require("vim.ui.clipboard.osc52").copy("+"),
-    ["*"] = require("vim.ui.clipboard.osc52").copy("*"),
-  },
-  paste = {
-    ["+"] = require("vim.ui.clipboard.osc52").paste("+"),
-    ["*"] = require("vim.ui.clipboard.osc52").paste("*"),
-  },
-}
+if vim.fn.has("nvim-0.10") == 1 then
+  vim.g.clipboard = {
+    name = "osc52",
+    copy = {
+      ["+"] = require("vim.ui.clipboard.osc52").copy("+"),
+      ["*"] = require("vim.ui.clipboard.osc52").copy("*"),
+    },
+    paste = {
+      ["+"] = require("vim.ui.clipboard.osc52").paste("+"),
+      ["*"] = require("vim.ui.clipboard.osc52").paste("*"),
+    },
+  }
+end
+
 
 vim.opt.termguicolors = true
 vim.diagnostic.config({ signs = false })
