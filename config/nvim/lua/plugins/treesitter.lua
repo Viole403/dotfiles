c3767880eb5b750cd884c12ee67b92e450d616ab
Viole403 <m.maulana.fb26@gmail.com>
2025-12-19 14:21:41 +0700

    style(treesitter): format code and enhance configuration
    - Reformat the ensure_installed list for better readability.
    - Add sync_install and auto_install options for improved functionality.
    - Enhance highlight configuration with additional_vim_regex_highlighting option.

--- config/nvim/lua/plugins/treesitter.lua
+++ config/nvim/lua/plugins/treesitter.lua
@@ -3,11 +3,16 @@ return {
   "nvim-treesitter/nvim-treesitter",
   build = ":TSUpdate",
   config = function()
-    require("nvim-treesitter.configs").setup({
+    require'nvim-treesitter.configs'.setup {
       ensure_installed = {
-        "bash","lua","python","go","java","php","rust","html","css","json"
+        "bash", "lua", "python", "go", "java", "php", "rust", "html", "css", "json"
       },
-      highlight = { enable = true },
-    })
+      sync_install = false,
+      auto_install = true,
+      highlight = {
+        enable = true,
+        additional_vim_regex_highlighting = false,
+      },
+    }
   end
 }
