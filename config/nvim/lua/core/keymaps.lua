bce7d567a8d9807346ec578c62d9545016d4a6c1
Viole403 <m.maulana.fb26@gmail.com>
2025-12-19 21:36:50 +0700

    feat(keymaps): add NvimTree toggle keymap
    - Introduced a new keymap for toggling the NvimTree file explorer using <leader>e.
    - This enhances navigation and file management within Neovim.

--- config/nvim/lua/core/keymaps.lua
+++ config/nvim/lua/core/keymaps.lua
@@ -3,6 +3,9 @@ vim.keymap.set("v", "<C-S-c>", '"+y')
 vim.keymap.set({ "n", "i" }, "<C-S-v>", '"+p')
 vim.keymap.set("i", "<C-S-v>", "<C-r>+")
 
+-- NvimTree toggle
+vim.keymap.set("n", "<leader>e", "<cmd>NvimTreeToggle<CR>", { desc = "Toggle file explorer" })
+
 vim.g.tmux_navigator_no_mappings = 1
 vim.keymap.set("n", "<C-h>", "<Cmd>TmuxNavigateLeft<CR>")
 vim.keymap.set("n", "<C-j>", "<Cmd>TmuxNavigateDown<CR>")
--- config/nvim/lua/plugins/treesitter.lua
+++ config/nvim/lua/plugins/treesitter.lua
@@ -10,7 +10,7 @@ return {
     configs.setup {
       -- Core parsers synchronized with lvim config
       ensure_installed = {
-        "bash", "lua", "python", "go", "java", "php", "rust", "html", "css", "json", "javascript", "typescript", "tsx", "yaml", "markdown",
+        "vim", "vimdoc", "bash", "lua", "python", "go", "java", "php", "rust", "html", "css", "json", "javascript", "typescript", "tsx", "yaml", "markdown",
         "scala", "org", "c", "ruby", "http"
       },
       sync_install = false,
