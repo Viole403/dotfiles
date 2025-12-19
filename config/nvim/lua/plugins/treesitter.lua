a11a161a1d6e26bc41dcdd6db03988255a0c5af2
Viole403 <m.maulana.fb26@gmail.com>
2025-12-19 20:38:16 +0700

    feat(nvim): add additional treesitter parsers
    - Add scala, org, c, ruby, http parsers
    - Synchronized with LunarVim parser list

--- config/nvim/lua/plugins/treesitter.lua
+++ config/nvim/lua/plugins/treesitter.lua
@@ -10,7 +10,8 @@ return {
     configs.setup {
       -- Core parsers synchronized with lvim config
       ensure_installed = {
-        "bash", "lua", "python", "go", "java", "php", "rust", "html", "css", "json", "javascript", "typescript", "tsx", "yaml", "markdown"
+        "bash", "lua", "python", "go", "java", "php", "rust", "html", "css", "json", "javascript", "typescript", "tsx", "yaml", "markdown",
+        "scala", "org", "c", "ruby", "http"
       },
       sync_install = false,
       auto_install = true,
