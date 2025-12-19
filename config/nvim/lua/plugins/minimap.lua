47b9b29f70f880593b8226e98dae3854585fdb85
Viole403 <m.maulana.fb26@gmail.com>
2025-12-19 20:38:10 +0700

    feat(nvim): enable minimap auto-start
    - Enable minimap_auto_start
    - Enable minimap_auto_start_win_enter
    - Add more minimap commands
    - Set lazy = false for immediate loading

--- config/nvim/lua/plugins/minimap.lua
+++ config/nvim/lua/plugins/minimap.lua
@@ -2,10 +2,12 @@
 return {
   "wfxr/minimap.vim",
   build = "cargo install --locked code-minimap",
-  cmd = { "Minimap", "MinimapToggle" },
+  cmd = { "Minimap", "MinimapClose", "MinimapToggle", "MinimapRefresh", "MinimapUpdateHighlight" },
+  lazy = false,
   config = function()
     vim.g.minimap_width = 10
-    vim.g.minimap_auto_start = 0
+    vim.g.minimap_auto_start = 1
+    vim.g.minimap_auto_start_win_enter = 1
     vim.g.minimap_highlight_search = 1
     vim.g.minimap_git_colors = 1
     vim.g.minimap_block_filetypes = { "NvimTree", "TelescopePrompt", "alpha", "dashboard" }
