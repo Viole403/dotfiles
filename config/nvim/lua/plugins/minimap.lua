d198f65f27a2a641a42b75f9c6def0568d1f4cae
Viole403 <m.maulana.fb26@gmail.com>
2025-12-19 22:09:29 +0700

    feat(minimap): enhance configuration for minimap plugin
    - Set minimap window side to 'right' for better visibility.
    - Block specific filetypes from displaying in the minimap.

--- config/nvim/lua/plugins/minimap.lua
+++ config/nvim/lua/plugins/minimap.lua
@@ -8,5 +8,7 @@ return {
     vim.cmd("let g:minimap_width = 10")
     vim.cmd("let g:minimap_auto_start = 1")
     vim.cmd("let g:minimap_auto_start_win_enter = 1")
+    vim.cmd("let g:minimap_window_side = 'right'")
+    vim.g.minimap_block_filetypes = { "NvimTree", "TelescopePrompt", "alpha", "dashboard" }
   end,
 }
