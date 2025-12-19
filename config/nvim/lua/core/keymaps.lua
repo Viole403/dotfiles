bd0c8e60ace58415ba19c80de707f3bc93d252ae
Viole403 <m.maulana.fb26@gmail.com>
2025-12-19 21:50:45 +0700

    refactor(keymaps): update NvimTree toggle keymap
    - Ensure consistency in the NvimTree toggle keymap definition.
    - No functional changes, just a minor adjustment for clarity.

--- config/nvim/lua/core/keymaps.lua
+++ config/nvim/lua/core/keymaps.lua
@@ -4,7 +4,7 @@ vim.keymap.set({ "n", "i" }, "<C-S-v>", '"+p')
 vim.keymap.set("i", "<C-S-v>", "<C-r>+")
 
 -- NvimTree toggle
--- vim.keymap.set("n", "<leader>e", "<cmd>NvimTreeToggle<CR>", { desc = "Toggle file explorer" })
+vim.keymap.set("n", "<leader>e", "<cmd>NvimTreeToggle<CR>", { desc = "Toggle file explorer" })
 
 vim.g.tmux_navigator_no_mappings = 1
 vim.keymap.set("n", "<C-h>", "<Cmd>TmuxNavigateLeft<CR>")
--- config/nvim/lua/plugins/bufferline.lua
+++ config/nvim/lua/plugins/bufferline.lua
@@ -3,9 +3,11 @@ return {
   "akinsho/bufferline.nvim",
   version = "*",
   dependencies = "nvim-tree/nvim-web-devicons",
+  lazy = false,
   config = function()
     require("bufferline").setup({
       options = {
+        mode = "buffers",
         show_buffer_icons = false,
         show_buffer_close_icons = false,
         show_close_icon = false,
@@ -19,7 +21,18 @@ return {
             separator = true,
           },
         },
+        diagnostics = "nvim_lsp",
+        diagnostics_indicator = function(count, level)
+          local icon = level:match("error") and " " or " "
+          return " " .. icon .. count
+        end,
       },
     })
+
+    -- Keybindings for buffer navigation
+    vim.keymap.set("n", "<Tab>", "<cmd>BufferLineCycleNext<CR>", { desc = "Next buffer" })
+    vim.keymap.set("n", "<S-Tab>", "<cmd>BufferLineCyclePrev<CR>", { desc = "Previous buffer" })
+    vim.keymap.set("n", "<leader>bd", "<cmd>bdelete<CR>", { desc = "Delete buffer" })
+    vim.keymap.set("n", "<leader>bp", "<cmd>BufferLinePick<CR>", { desc = "Pick buffer" })
   end,
 }
