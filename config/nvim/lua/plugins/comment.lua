051b3b19f65bcd2eae278b1f727db4d8765b19db
Viole403 <m.maulana.fb26@gmail.com>
2025-12-20 17:02:31 +0700

    feat(comment): add Comment.nvim plugin configuration
    - Introduce configuration for the Comment.nvim plugin.
    - Set up key mappings for line and block comments.
    - Enable padding and sticky options for better usability.
    - Define extra mappings for commenting above, below, and at the end of the line.

---
+++ config/nvim/lua/plugins/comment.lua
@@ -0,0 +1,29 @@
+-- plugins/comment.lua
+return {
+  "numToStr/Comment.nvim",
+  event = { "BufReadPost", "BufNewFile" },
+  config = function()
+    require("Comment").setup({
+      padding = true,
+      sticky = true,
+      ignore = nil,
+      toggler = {
+        line = "gcc",
+        block = "gbc",
+      },
+      opleader = {
+        line = "gc",
+        block = "gb",
+      },
+      extra = {
+        above = "gcO",
+        below = "gco",
+        eol = "gcA",
+      },
+      mappings = {
+        basic = true,
+        extra = true,
+      },
+    })
+  end,
+}
