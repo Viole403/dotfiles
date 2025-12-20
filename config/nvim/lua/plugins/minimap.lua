1907c9afdc44d19767eb8e03fbf0d31f2f254666
Viole403 <m.maulana.fb26@gmail.com>
2025-12-20 09:48:19 +0700

    refactor(minimap): update minimap plugin configuration
    - Replace the old minimap plugin with 'echasnovski/mini.map'.
    - Configure the new minimap with integrations for search and diagnostics.
    - Set up symbols and window properties for the minimap.
    - Add optional key mapping for toggling the minimap.
    - Enable auto-show for all buffers on startup.

--- config/nvim/lua/plugins/minimap.lua
+++ config/nvim/lua/plugins/minimap.lua
@@ -1,20 +1,30 @@
 return {
-  "wfxr/minimap.vim",
-  cmd = {
-    "Minimap",
-    "MinimapToggle",
-    "MinimapClose",
-  },
-  init = function()
-    vim.g.minimap_width = 10
-    vim.g.minimap_auto_start = 0
-    vim.g.minimap_highlight_search = 1
-    vim.g.minimap_git_colors = 1
-    vim.g.minimap_block_filetypes = {
-      "NvimTree",
-      "TelescopePrompt",
-      "alpha",
-      "dashboard",
-    }
+  "echasnovski/mini.map",
+  lazy = false,
+  config = function()
+    local map = require("mini.map")
+
+    map.setup({
+      integrations = {
+        map.gen_integration.builtin_search(),
+        map.gen_integration.diagnostic({ error = "ErrorMsg", warn = "WarningMsg", info = "None", hint = "None" }),
+      },
+      symbols = {
+        encode = map.gen_encode_symbols.dot("4x2"),
+      },
+      window = {
+        side = "right",
+        width = 10,
+        winblend = 0,
+        show_integration_count = false,
+      },
+      on_click = "scroll",
+    })
+
+    -- Optional: toggle mapping
+    vim.keymap.set("n", "<leader>mm", map.toggle, { desc = "Toggle MiniMap" })
+
+    -- Optional: auto-show for all buffers
+    map.open()
   end,
 }
