6bd0da7a06e243a6bae033ab5c7318c0723c9bc1
Viole403 <m.maulana.fb26@gmail.com>
2025-12-21 16:22:29 +0700

    refactor(nvim): organize navigation plugins into navigation/ directory
    - Move nvimtree, oil, project, spectre, telescope, trouble
    - Update file headers to reflect new location (plugins/navigation/*)
    - Group file and project navigation tools for clarity

---
+++ config/nvim/lua/plugins/navigation/oil.lua
@@ -0,0 +1,18 @@
+-- plugins/navigation/oil.lua
+return {
+  "stevearc/oil.nvim",
+  dependencies = { "nvim-tree/nvim-web-devicons" },
+  config = function()
+    require("oil").setup({
+      default_file_explorer = false,
+      columns = {
+        "icon",
+      },
+      view_options = {
+        show_hidden = true,
+      },
+    })
+
+    vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
+  end,
+}
---
+++ config/nvim/lua/plugins/navigation/trouble.lua
@@ -0,0 +1,27 @@
+-- plugins/navigation/trouble.lua
+return {
+  "folke/trouble.nvim",
+  cmd = "Trouble",
+  keys = {
+    { "<leader>xx", "<cmd>Trouble diagnostics toggle<cr>", desc = "Diagnostics (Trouble)" },
+    { "<leader>xX", "<cmd>Trouble diagnostics toggle filter.buf=0<cr>", desc = "Buffer Diagnostics" },
+    { "<leader>cs", "<cmd>Trouble symbols toggle focus=false<cr>", desc = "Symbols (Trouble)" },
+    { "<leader>cl", "<cmd>Trouble lsp toggle focus=false win.position=right<cr>", desc = "LSP Definitions" },
+    { "<leader>xL", "<cmd>Trouble loclist toggle<cr>", desc = "Location List" },
+    { "<leader>xQ", "<cmd>Trouble qflist toggle<cr>", desc = "Quickfix List" },
+  },
+  opts = {
+    use_diagnostic_signs = false,
+    icons = false,
+    fold_open = "v",
+    fold_closed = ">",
+    indent_lines = false,
+    signs = {
+      error = "E",
+      warning = "W",
+      hint = "H",
+      information = "I",
+      other = "?",
+    },
+  },
+}
